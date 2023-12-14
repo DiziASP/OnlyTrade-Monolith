1c7bc2c69457092a350728ec559a448aa23c1110
ReyhanPA <87368608+ReyhanPA@users.noreply.github.com>
2023-12-14 23:50:09 +0700

    feat: add catalogue and detail

--- app/Views/catalogue.php
+++ app/Views/catalogue.php
@@ -11,94 +11,85 @@
 			<!-- Products tab & slick -->
 			<div class="col-md-12">
 				<div class="row">
-					<div class="products-tabs">
-						<!-- tab -->
-						<div id="tab1" class="tab-pane active">
-							<div class="products-slick" data-nav="#slick-nav-1">
-								<!-- product -->
-								<div class="product">
-									<div class="product-img">
-										<img src="<?php echo base_url()?>/template/img/product01.png" alt="">
-									</div>
-									<div class="product-body">
-										<p class="product-category">Category</p>
-										<h3 class="product-name"><a href="#">product name goes here</a></h3>
-										<h4 class="product-price">$980.00</h4>
-									</div>
-									<div class="add-to-cart">
-										<a href="/detail"><button class="add-to-cart-btn"><i class="fa fa-shopping-cart"></i>check detail</button></a>
-									</div>
-								</div>
-								<!-- /product -->
-								
-								<!-- product -->
-								<div class="product">
-									<div class="product-img">
-										<img src="<?php echo base_url()?>/template/img/product01.png" alt="">
-									</div>
-									<div class="product-body">
-										<p class="product-category">Category</p>
-										<h3 class="product-name"><a href="#">product name goes here</a></h3>
-										<h4 class="product-price">$980.00</h4>
-									</div>
-									<div class="add-to-cart">
-										<a href="/detail"><button class="add-to-cart-btn"><i class="fa fa-shopping-cart"></i>check detail</button></a>
-									</div>
-								</div>
-								<!-- /product -->
-								
-								<!-- product -->
-								<div class="product">
-									<div class="product-img">
-										<img src="<?php echo base_url()?>/template/img/product01.png" alt="">
-									</div>
-									<div class="product-body">
-										<p class="product-category">Category</p>
-										<h3 class="product-name"><a href="#">product name goes here</a></h3>
-										<h4 class="product-price">$980.00</h4>
-									</div>
-									<div class="add-to-cart">
-										<a href="/detail"><button class="add-to-cart-btn"><i class="fa fa-shopping-cart"></i>check detail</button></a>
-									</div>
-								</div>
-								<!-- /product -->
+                    <!-- Product -->
+                    <div class="col-md-3">
+                        <div class="product">
+                            <div class="product-img">
+                                <img src="<?php echo base_url()?>/template/img/product01.png" alt="">
+                            </div>
+                            <div class="product-body">
+                                <p class="product-category">Category</p>
+                                <h3 class="product-name"><a href="#">Product Name Goes Here</a></h3>
+                                <h4 class="product-price">$980.00</h4>
+                            </div>
+                            <div class="add-to-cart">
+                                <a href="/detail">
+                                    <button class="add-to-cart-btn"><i class="fa fa-shopping-cart"></i>Check Detail</button>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- /Product -->
 
-								<!-- product -->
-								<div class="product">
-									<div class="product-img">
-										<img src="<?php echo base_url()?>/template/img/product01.png" alt="">
-									</div>
-									<div class="product-body">
-										<p class="product-category">Category</p>
-										<h3 class="product-name"><a href="#">product name goes here</a></h3>
-										<h4 class="product-price">$980.00</h4>
-									</div>
-									<div class="add-to-cart">
-										<a href="/detail"><button class="add-to-cart-btn"><i class="fa fa-shopping-cart"></i>check detail</button></a>
-									</div>
-								</div>
-								<!-- /product -->
-								
-								<!-- product -->
-								<div class="product">
-									<div class="product-img">
-										<img src="<?php echo base_url()?>/template/img/product01.png" alt="">
-									</div>
-									<div class="product-body">
-										<p class="product-category">Category</p>
-										<h3 class="product-name"><a href="#">product name goes here</a></h3>
-										<h4 class="product-price">$980.00</h4>
-									</div>
-									<div class="add-to-cart">
-										<a href="/detail"><button class="add-to-cart-btn"><i class="fa fa-shopping-cart"></i>check detail</button></a>
-									</div>
-								</div>
-								<!-- /product -->
-							</div>
-							<div id="slick-nav-1" class="products-slick-nav"></div>
-						</div>
-						<!-- /tab -->
-					</div>
+                    <!-- Product -->
+                    <div class="col-md-3">
+                        <div class="product">
+                            <div class="product-img">
+                                <img src="<?php echo base_url()?>/template/img/product01.png" alt="">
+                            </div>
+                            <div class="product-body">
+                                <p class="product-category">Category</p>
+                                <h3 class="product-name"><a href="#">Product Name Goes Here</a></h3>
+                                <h4 class="product-price">$980.00</h4>
+                            </div>
+                            <div class="add-to-cart">
+                                <a href="/detail">
+                                    <button class="add-to-cart-btn"><i class="fa fa-shopping-cart"></i>Check Detail</button>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- /Product -->
+
+                    <!-- Product -->
+                    <div class="col-md-3">
+                        <div class="product">
+                            <div class="product-img">
+                                <img src="<?php echo base_url()?>/template/img/product01.png" alt="">
+                            </div>
+                            <div class="product-body">
+                                <p class="product-category">Category</p>
+                                <h3 class="product-name"><a href="#">Product Name Goes Here</a></h3>
+                                <h4 class="product-price">$980.00</h4>
+                            </div>
+                            <div class="add-to-cart">
+                                <a href="/detail">
+                                    <button class="add-to-cart-btn"><i class="fa fa-shopping-cart"></i>Check Detail</button>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- /Product -->
+					
+                    <!-- Product -->
+                    <div class="col-md-3">
+                        <div class="product">
+                            <div class="product-img">
+                                <img src="<?php echo base_url()?>/template/img/product01.png" alt="">
+                            </div>
+                            <div class="product-body">
+                                <p class="product-category">Category</p>
+                                <h3 class="product-name"><a href="#">Product Name Goes Here</a></h3>
+                                <h4 class="product-price">$980.00</h4>
+                            </div>
+                            <div class="add-to-cart">
+                                <a href="/detail">
+                                    <button class="add-to-cart-btn"><i class="fa fa-shopping-cart"></i>Check Detail</button>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- /Product -->
 				</div>
 			</div>
 			<!-- Products tab & slick -->
